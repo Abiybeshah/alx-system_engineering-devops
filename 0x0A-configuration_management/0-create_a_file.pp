0b632838974373730ef14d9df615d667bf5eb946
Abiybeshah <abiyjem99@gmail.com>
2024-06-29 01:49:45 +0300

    create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 #creating a file in /tmp puppet
+
 file {'school stuff':
-  path     => 'tmp/school',
-  mode     => '0744',
-  owner    => 'www-data',
-  group    => 'www-data',
-  content  => 'I love Puppet',
+  path    => 'tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
